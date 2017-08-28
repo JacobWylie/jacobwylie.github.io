cb5144ff8387cfe1086e4608ffa3f3ece7e180f0
Jacob Wylie <jacobwylie@gmail.com>
2017-08-28 22:09:06 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -2,3 +2,18 @@
 particlesJS.load('particles-js', 'particles-js/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+// Hide/show portfolio projects
+function portfolio() {
+    const y = document.querySelector('.links');
+    var x = document.querySelector('.portfolio');
+    if (y.classList.contains('is-hidden')) {
+        y.classList.remove('is-hidden', 'fadeOutDownBig');
+        y.classList.add('fadeInDownBig');
+        x.classList.add('is-hidden');
+    } else {
+        x.classList.remove('is-hidden');
+        y.classList.add('is-hidden', 'fadeOutDownBig');
+        y.classList.remove('fadeInDownBig')
+    }
+}
