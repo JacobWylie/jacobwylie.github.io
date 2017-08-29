3e2d5a1afdc3f6ee3acdd92c05e64f0907eb2b5a
JacobWylie <jacobwylie@gmail.com>
2017-08-29 12:38:59 +0200

    added js to hide and show link divs

--- app.js
+++ app.js
@@ -3,20 +3,79 @@ particlesJS.load('particles-js', 'particles-js/particlesjs-config.json', functio
   console.log('callback - particles.js config loaded');
 });
 
-// Hide/show portfolio projects
-function portfolio() {
-    const y = document.querySelector('.links');
-    var x = document.querySelector('.portfolio');
-    const z = document.querySelector('.hint');
-    if (y.classList.contains('is-hidden')) {
-        y.classList.remove('is-hidden', 'fadeOutDownBig');
-        z.classList.remove('is-hidden');
-        y.classList.add('fadeInDownBig');
-        x.classList.add('is-hidden');
-    } else {
-        x.classList.remove('is-hidden');
-        y.classList.add('is-hidden', 'fadeOutDownBig');
-        z.classList.add('is-hidden');
-        y.classList.remove('fadeInDownBig')
+const onlineBtn = document.querySelector('.online-btn');
+const portfolioBtn = document.querySelector('.portfolio-btn');
+const contactBtn = document.querySelector('.contact-btn');
+const onlineDiv = document.querySelector('.online');
+const portfolioDiv = document.querySelector('.portfolio');
+const contactDiv = document.querySelector('.contact');
+const hintDiv = document.querySelector('.hint');
+
+function showOnline() {
+    if (onlineDiv.classList.contains('is-hidden')) {
+        onlineDiv.classList.remove('is-hidden');
+    }
+    if (!portfolioDiv.classList.contains('is-hidden')) {
+        portfolioDiv.classList.add('is-hidden')
+    }
+    if (!contactDiv.classList.contains('is-hidden')) {
+        contactDiv.classList.add('is-hidden')
+    }
+    if (hintDiv.classList.contains('is-hidden')) {
+        hintDiv.classList.remove('is-hidden')
     }
 }
+
+function showPortfolio() {
+    if (portfolioDiv.classList.contains('is-hidden')) {
+        portfolioDiv.classList.remove('is-hidden');
+    }
+    if (!onlineDiv.classList.contains('is-hidden')) {
+        onlineDiv.classList.add('is-hidden')
+    }
+    if (!contactDiv.classList.contains('is-hidden')) {
+        contactDiv.classList.add('is-hidden')
+    }
+    if (!hintDiv.classList.contains('is-hidden')) {
+        hintDiv.classList.add('is-hidden')
+    }
+}
+
+function showContact() {
+    if (contactDiv.classList.contains('is-hidden')) {
+        contactDiv.classList.remove('is-hidden');
+    }
+    if (!onlineDiv.classList.contains('is-hidden')) {
+        onlineDiv.classList.add('is-hidden')
+    }
+    if (!portfolioDiv.classList.contains('is-hidden')) {
+        portfolioDiv.classList.add('is-hidden')
+    }
+    if (!hintDiv.classList.contains('is-hidden')) {
+        hintDiv.classList.add('is-hidden')
+    }
+}
+
+
+onlineBtn.addEventListener('click', showOnline);
+portfolioBtn.addEventListener('click', showPortfolio);
+contactBtn.addEventListener('click', showContact);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
